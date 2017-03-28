e165ddc7d509d8e6f4a904c0272cabd90eb80adb
dubech <franck.dubech@hotmail.com>
2017-03-28 12:06:12 +0200

    exo 3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 $('#removeDiv').click(function(){ 
 	$('#remove').remove() })
 
+$('#setText').val(function(){
+	
+}
+
+
+$('#setColor').click(function(){
+	$('#changeColor').css("background-color", "red")})
